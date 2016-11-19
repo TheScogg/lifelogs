3867c276ceed21e23ea84261bc581b8cb7bd4e62
James Scoggins <thescogg@gmail.com>
2016-11-19 10:57:49 -0500

    ...1

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var database = require('./public/javascripts/database');
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://lifelogs.herokuapp.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
